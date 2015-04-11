b567a6a1a80be62d4f294ee7100e7120cd2b61f9
Ezard <ezard.ben@gmail.com>
2015-04-11 20:54:30 +0100

    Lots of stuff

--- app/src/main/java/com/ezardlabs/cropop/Main.java
+++ app/src/main/java/com/ezardlabs/cropop/Main.java
@@ -1,30 +1,89 @@
 package com.ezardlabs.cropop;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.ezardlabs.cropop.contacts.Contacts;
-import com.ezardlabs.cropop.data.Data;
+import com.ezardlabs.cropop.contacts.ContactsFragment;
+import com.ezardlabs.cropop.notifications.NotificationsFragment;
 
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import butterknife.InjectView;
 
 public class Main extends Activity {
+	@InjectView(R.id.drawer_layout) DrawerLayout drawerLayout;
+	@InjectView(R.id.listView) ListView drawerListView;
+	private ActionBarDrawerToggle drawerToggle;
+	private int position = 0;
+	private String[] items = {"Contacts", "Notifications", "Settings"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		ButterKnife.inject(this);
+		drawerLayout = ((DrawerLayout) findViewById(R.id.drawer_layout));
+		drawerListView = ((ListView) findViewById(R.id.listView));
+		drawerListView.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, items));
+		drawerListView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Main.this.selectItem(position);
+			}
+		});
+		drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		getSupportActionBar().setDisplayShowHomeEnabled(true);
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.app_name, R.string.app_name) {
+			public void onDrawerClosed(View drawerView) {
+				Main.this.supportInvalidateOptionsMenu();
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				Main.this.supportInvalidateOptionsMenu();
+			}
+		};
+		drawerLayout.setDrawerListener(drawerToggle);
+		selectItem(0);
 	}
 
-	@OnClick(R.id.contacts)
-	void contactsClick() {
-		startActivity(new Intent(this, Contacts.class));
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		drawerToggle.syncState();
 	}
 
-	@OnClick(R.id.data)
-	void dataClick() {
-		startActivity(new Intent(this, Data.class));
+	public boolean onOptionsItemSelected(MenuItem item) {
+		DrawerLayout localDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		ListView localListView = (ListView) findViewById(R.id.listView);
+		if (localDrawerLayout.isDrawerOpen(localListView)) localDrawerLayout.closeDrawer(localListView);
+		else localDrawerLayout.openDrawer(localListView);
+		return true;
+	}
+
+	void selectItem(int position) {
+		this.position = position;
+		Fragment fragment = null;
+		switch (position) {
+			case 0:
+				fragment = new ContactsFragment();
+				break;
+			case 1:
+				fragment = new NotificationsFragment();
+				break;
+		}
+		if (fragment != null) {
+			getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();
+		}
+		drawerListView.setItemChecked(position, true);
+		getSupportActionBar().setTitle(items[position]);
+		drawerLayout.closeDrawer(drawerListView);
 	}
 }
--- app/src/main/java/com/ezardlabs/cropop/sms/SmsReceiver.java
+++ app/src/main/java/com/ezardlabs/cropop/sms/SmsReceiver.java
@@ -1,11 +1,15 @@
 package com.ezardlabs.cropop.sms;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
+
+import com.ezardlabs.cropop.DBManager;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,31 +21,41 @@ public class SmsReceiver extends BroadcastReceiver {
 		Object[] pdus = (Object[]) pdusBundle.get("pdus");
 		SmsMessage messages = SmsMessage.createFromPdu((byte[]) pdus[0]);
 		Log.i("", messages.getMessageBody() + ", " + messages.getOriginatingAddress());
-		Pattern pattern = Pattern.compile("((DIS) ((?:A[1-3])|UNKNOWN))|((INC) ([0-9]+(?:\\.[0-9]+)?))|((HRV) ([0-9]+(?:\\.[0-9]+)?C[1-3])\\s?+)");
+		Pattern pattern = Pattern.compile("(?:(DIS) ((?:A[1-3])|UNKNOWN))|(?:(INC) ([0-9]+(?:\\.[0-9]+)?))|(?:(HRV)\\s(([0-9]+(?:\\.[0-9]+)?C[1-3]\\s?)+))");
 		Matcher m = pattern.matcher(messages.getMessageBody());
 		while (m.find()) {
-			Log.i("", "Start");
 			outer:
 			for (int i = 1; i < m.groupCount(); i++) {
 				if (m.group(i) != null) {
 					switch (m.group(i)) {
 						case "DIS":
 							Log.i("", "Disease type: " + m.group(i + 1));
+							DBManager.addDisease(messages.getOriginatingAddress(), m.group(i + 1));
+							if (m.group(i + 1).equals("UNKNOWN")) {
+								createNotification(context, "Unknown disease reported");
+							}
 							break outer;
 						case "INC":
 							Log.i("", "Income: $" + Float.parseFloat(m.group(i + 1)) * 1000f);
+							DBManager.addIncome(messages.getOriginatingAddress(), Float.parseFloat(m.group(i + 1)) * 1000f);
 							break outer;
-//						case "HRV":
-//							Log.i("", "Harvest: " + (Float.parseFloat(m.group(i + 1)) * 1000f) + "kg");
-//							break outer;
+						case "HRV":
+							String[] split = m.group(i + 1).split(" ");
+							for (String s : split) {
+								String[] split2 = s.split("C");
+								DBManager.addHarvest(messages.getOriginatingAddress(), Float.parseFloat(split2[0]) * 1000f, Integer.parseInt(split2[1]));
+							}
+							break outer;
 					}
 				}
 				Log.i("", "m.group(" + i + "): " + m.group(i));
 			}
 		}
-//		if (m.find()) {
-//			Log.i("", m.group(count++));
 		abortBroadcast();
-//		}
+	}
+
+	private void createNotification(Context context, String description) {
+		((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE))
+				.notify(1234, new NotificationCompat.Builder(context).setContentTitle("CropOp Alert").setContentText(description).build());
 	}
 }
