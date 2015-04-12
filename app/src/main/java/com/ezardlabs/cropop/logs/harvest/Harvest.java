ff7a2673d11874e9e4c5a5388c83a3ea69d71263
ram534 <ram534@york.ac.uk>
2015-04-12 10:51:22 +0100

    yolo indeed

--- app/src/main/java/com/ezardlabs/cropop/logs/harvest/Harvest.java
+++ app/src/main/java/com/ezardlabs/cropop/logs/harvest/Harvest.java
@@ -2,12 +2,16 @@ package com.ezardlabs.cropop.logs.harvest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.ezardlabs.cropop.Activity;
+import com.ezardlabs.cropop.DBManager;
 import com.ezardlabs.cropop.R;
 
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
 
 /**
@@ -20,5 +24,5 @@ public class Harvest extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.harvest);
         ButterKnife.inject(this);
-    }
+            }
 }
